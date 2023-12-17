c9e4fb4bc7d1c3f380f8a484678e2be1f1560b7d
DamirSul <suleimenov.damirrr@mail.ru>
2023-12-17 23:15:07 +0600

    add project too

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="w-96 mx-auto pt-8">
+        <h1 class="text-xl mb-8">My Posts Site</h1>
+
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
